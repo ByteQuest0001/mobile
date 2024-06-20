f2ff877a290451f47e3f83ff48082621d9e93b14
ro77en <lucas.mdantas@icloud.com>
2024-06-20 10:33:21 -0300

    enabling appbar on home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import "package:bytequest/helper/global.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
   @override
   Widget build(BuildContext context) {
     // Initializing device size
