a9b35f7d262accc6335851924a0f854657eb0267
ro77en <lucas.mdantas@icloud.com>
2024-06-20 11:49:03 -0300

    overflow fixes

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -23,109 +23,111 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
         backgroundColor: const Color(0xFF1A0047),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // For some spacing
-              SizedBox(
-                height: mq.height * .09,
-              ),
-
-              Center(
-                child: Image.asset(
-                  'assets/images/icon.png',
-                  height: mq.height * .3,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // For some spacing
+                SizedBox(
+                  height: mq.height * .09,
                 ),
-              ),
-              // For some spacing
-              SizedBox(
-                height: mq.height * .04,
-              ),
 
-              const TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  labelText: 'Username',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                Center(
+                  child: Image.asset(
+                    'assets/images/icon.png',
+                    height: mq.height * .3,
                   ),
                 ),
-                style: TextStyle(color: Colors.white),
-              ),
+                // For some spacing
+                SizedBox(
+                  height: mq.height * .04,
+                ),
 
-              // For some spacing
-              SizedBox(
-                height: mq.height * .04,
-              ),
-
-              const TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                const TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Username',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                  style: TextStyle(color: Colors.white),
+                ),
+
+                // For some spacing
+                SizedBox(
+                  height: mq.height * .04,
+                ),
+
+                const TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Forgot my Password',
+                    style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.white),
                   ),
                 ),
-                style: TextStyle(color: Colors.white),
-              ),
 
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Forgot my Password',
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.white),
+                SizedBox(
+                  height: mq.height * .05,
                 ),
-              ),
 
-              SizedBox(
-                height: mq.height * .05,
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  // login
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    minimumSize: Size(mq.width, 50)),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.black),
+                ElevatedButton(
+                  onPressed: () {
+                    // login
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      minimumSize: Size(mq.width, 50)),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
-              ),
 
-              SizedBox(
-                height: mq.height * .05,
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  // sign up
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    minimumSize: Size(mq.width, 50)),
-                child: const Text(
-                  'Sign Up',
-                  style: TextStyle(color: Colors.black),
+                SizedBox(
+                  height: mq.height * .05,
                 ),
-              ),
-            ],
+
+                ElevatedButton(
+                  onPressed: () {
+                    // sign up
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      minimumSize: Size(mq.width, 50)),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
