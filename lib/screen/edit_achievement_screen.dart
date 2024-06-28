699c0dfc97a1b89381a8ea0790d785e34f5c1157
ro77en <lucas.mdantas@icloud.com>
2024-06-28 17:32:30 -0300

    edit achievement screen layout

--- lib/screen/edit_achievement_screen.dart
+++ lib/screen/edit_achievement_screen.dart
@@ -1,3 +1,6 @@
+import 'package:bytequest/helper/global.dart';
+import 'package:bytequest/screen/achievements_screen.dart';
+import 'package:bytequest/screen/user_profile_screen.dart';
 import 'package:flutter/material.dart';
 import '../model/achievement_model.dart';
 import '../controller/achievement_controller.dart';
@@ -62,41 +65,240 @@ class _EditAchievementScreenState extends State<EditAchievementScreen> {
 
   @override
   Widget build(BuildContext context) {
+    mq = MediaQuery.sizeOf(context);
+    const bgColor = Color(0xFF1A0047);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Achievement'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: _saveAchievement,
+      backgroundColor: bgColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 70),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // back button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AchievementsScreen(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.arrow_back),
+                    color: Colors.white,
+                  ),
+
+                  // save and user profile icon
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: _saveAchievement,
+                            icon: const Icon(
+                              Icons.save,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const UserProfileScreen(),
+                                ),
+                              );
+                            },
+                            child: Image.asset(
+                              'assets/images/user.png',
+                              height: 50,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              // spacing
+              const SizedBox(
+                height: 50,
+              ),
+
+              // name
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: mq.width * .9,
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    child: Center(
+                      child: TextField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Name',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // spacing
+              const SizedBox(
+                height: 30,
+              ),
+
+              // description
+              Container(
+                height: mq.height * .2,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: bgColor,
+                  border: Border.all(
+                    color: Colors.white,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: TextField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(
+                      labelText: 'Description',
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+
+              // spacing
+              const SizedBox(height: 50),
+
+              // date + situation row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _dateController,
+                        decoration: const InputDecoration(
+                          labelText: 'Date',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: const TextStyle(
+                            color: Colors.white), // Set text color to white
+                      ),
+                    ),
+                  ),
+
+                  // spacing
+                  const SizedBox(height: 10),
+
+                  // situation
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _situationController,
+                        decoration: const InputDecoration(
+                          labelText: 'Situation',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: const TextStyle(
+                            color: Colors.white), // Set text color to white
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // date
+
+              // spacing
+              const SizedBox(height: 40),
+
+              // category
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Category: ',
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      child: TextField(
+                        controller: _categoryController,
+                        decoration: const InputDecoration(
+                          labelText: 'Category',
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                        style: TextStyle(
+                            color: Colors.white), // Set text color to white
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Name'),
-            ),
-            TextField(
-              controller: _descriptionController,
-              decoration: const InputDecoration(labelText: 'Description'),
-            ),
-            TextField(
-              controller: _dateController,
-              decoration: const InputDecoration(labelText: 'Date'),
-            ),
-            TextField(
-              controller: _situationController,
-              decoration: const InputDecoration(labelText: 'Situation'),
-            ),
-            TextField(
-              controller: _categoryController,
-              decoration: const InputDecoration(labelText: 'Category'),
-            ),
-          ],
         ),
       ),
     );
