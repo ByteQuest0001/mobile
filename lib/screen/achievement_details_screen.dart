5a4ed73eceec23b827c41ebbca7156a9177f6b91
ro77en <lucas.mdantas@icloud.com>
2024-06-28 12:04:03 -0300

    achievement details layout fix

--- lib/screen/achievement_details_screen.dart
+++ lib/screen/achievement_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bytequest/helper/global.dart';
 import 'package:bytequest/model/achievement_model.dart';
 import 'package:bytequest/screen/achievements_screen.dart';
 import 'package:bytequest/screen/user_profile_screen.dart';
@@ -10,13 +11,16 @@ class AchievementDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    mq = MediaQuery.sizeOf(context);
+    const bgColor = Color(0xFF1A0047);
+
     return Scaffold(
-      backgroundColor: const Color(0xFF1A0047),
+      backgroundColor: bgColor,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 50),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 70),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               // back button
               Row(
@@ -55,40 +59,146 @@ class AchievementDetailScreen extends StatelessWidget {
 
               // spacing
               const SizedBox(
+                height: 50,
+              ),
+
+              // name
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: mq.width * .9,
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        achievement.name,
+                        style: const TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // spacing
+              const SizedBox(
                 height: 30,
               ),
 
               // achievement description
-              Text(
-                'Description: ${achievement.description}',
-                style: const TextStyle(fontSize: 26, color: Colors.white),
+              Container(
+                height: mq.height * .2,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: bgColor,
+                    border: Border.all(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(
+                    achievement.description,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
 
               // spacing
-              const SizedBox(height: 10),
+              const SizedBox(height: 50),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text(
+                    'Date: ',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                    child: Text(
+                      achievement.date,
+                      style: const TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+
+                  // spacing
+                  const SizedBox(height: 10),
+
+                  // situation
+                  const Text(
+                    'Situation: ',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                    child: Text(
+                      achievement.situation,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ],
+              ),
 
               // date
-              Text(
-                'Date: ${achievement.date}',
-                style: const TextStyle(fontSize: 26, color: Colors.white),
-              ),
 
               // spacing
-              const SizedBox(height: 10),
-
-              // situation
-              Text(
-                'Situation: ${achievement.situation}',
-                style: const TextStyle(fontSize: 26, color: Colors.white),
-              ),
-
-              // spacing
-              const SizedBox(height: 10),
+              const SizedBox(height: 40),
 
               // category
-              Text(
-                'Category: ${achievement.category}',
-                style: const TextStyle(fontSize: 26, color: Colors.white),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Category: ',
+                    style: const TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      achievement.category,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
