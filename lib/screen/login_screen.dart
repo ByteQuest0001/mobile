a79f1cbeb360b4365224984a10cb18efbfce73c0
ro77en <lucas.mdantas@icloud.com>
2024-06-28 10:38:04 -0300

    skipping http request

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,5 +1,6 @@
 import "package:bytequest/controller/login_controller.dart";
 import "package:bytequest/helper/global.dart";
+import "package:bytequest/screen/home_screen.dart";
 import "package:bytequest/screen/signup_screen.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
@@ -102,7 +103,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
 
                 ElevatedButton(
-                  onPressed: () => _loginController.login(context),
+                  // onPressed: () => _loginController.login(context),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
                       minimumSize: Size(mq.width, 50)),
